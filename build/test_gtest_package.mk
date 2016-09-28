92c864c4dc32ea3542d752137cd58e1351165239
Guang Zhu <guangzhu@google.com>
2016-09-27 19:01:39 -0700

    remove CTS test manifest XML generation build rules
    Bug: 31754426
    Bug: 31788743
    Test: ...
      $ make ... cts
      # run command above before and after the change, and
      # check that generated out/host/linux-x86/cts/android-cts
      # folder contain same list of files (recursively)
      $ make ... checkbuild
      $ make ... CtsAnimationTestCases
      $ cts-tradefed run collect-tests-only \
        --skip-preconditions --skip-device-info \
        --skip-connectivity-check --primary-abi-only
      # run command above before and after the change, and
      # verify that test list is identical
    
    Change-Id: Ief157740fd2301fb78cc4e5a9754a2af8d0ffb37

--- build/test_gtest_package.mk
+++ build/test_gtest_package.mk
@@ -25,29 +25,3 @@ LOCAL_PROGUARD_ENABLED := disabled
 
 include $(BUILD_CTS_SUPPORT_PACKAGE)
 include $(BUILD_CTS_MODULE_TEST_CONFIG)
-
-cts_package_xml := $(CTS_TESTCASES_OUT)/$(LOCAL_PACKAGE_NAME).xml
-$(cts_package_xml): PRIVATE_PATH := $(LOCAL_PATH)
-$(cts_package_xml): PRIVATE_TEST_PACKAGE := android.$(notdir $(LOCAL_PATH))
-$(cts_package_xml): PRIVATE_EXECUTABLE := $(LOCAL_MODULE)
-$(cts_package_xml): PRIVATE_MANIFEST := $(LOCAL_PATH)/AndroidManifest.xml
-$(cts_package_xml): PRIVATE_TEST_LIST := $(LOCAL_PATH)/$(LOCAL_MODULE)_list.txt
-$(cts_package_xml): $(LOCAL_PATH)/$(LOCAL_MODULE)_list.txt
-$(cts_package_xml): $(cts_support_apks)
-$(cts_package_xml): $(cts_module_test_config)
-$(cts_package_xml): $(addprefix $(LOCAL_PATH)/,$(LOCAL_SRC_FILES))  $(CTS_NATIVE_TEST_SCANNER) $(CTS_XML_GENERATOR)
-	$(hide) echo Generating test description for wrapped native package $(PRIVATE_EXECUTABLE)
-	$(hide) mkdir -p $(CTS_TESTCASES_OUT)
-	$(hide) cat $(PRIVATE_TEST_LIST) | \
-			$(CTS_NATIVE_TEST_SCANNER) -t $(PRIVATE_TEST_PACKAGE) | \
-			$(CTS_XML_GENERATOR) -t wrappednative \
-						-m $(PRIVATE_MANIFEST) \
-						-n $(PRIVATE_EXECUTABLE) \
-						-p $(PRIVATE_TEST_PACKAGE) \
-						-e $(CTS_EXPECTATIONS) \
-						-b $(CTS_UNSUPPORTED_ABIS) \
-						-a $(CTS_TARGET_ARCH) \
-						-o $@
-
-# Have the module name depend on the cts files; so the cts files get generated when you run mm/mmm/mma/mmma.
-$(my_register_name) : $(cts_package_xml) $(cts_module_test_config)
